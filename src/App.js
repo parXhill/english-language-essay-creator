0f3d6f9e6203898e6f9966bc7a75d0fa252f0fa0
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-08-02 13:09:25 +1000

    gptRequestString is complete, though renders quite a few times

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import './App.css';
 import Components from './Components.js'
+import PromptTemplate from './PromptTemplate.js';
 import React, {useState} from 'react';
-import {promptTemplate} from './promptTemplate';
-
-//console.log(promptTemplate)
-
-
-
-
 
 function App() {
-
-
 
 
   const [activePage, setActivePage] = useState(0);
   const [examplesData, setExamplesData] = useState({});
   const [essayPrompt, setEssayPrompt] = useState("");
+  const [gptRequestString, setGPTRequestString] = useState("");
 
-  console.log(examplesData);
-  console.log(essayPrompt)
-
+  console.log(gptRequestString);
 
   function getEssayExamples(formData){
     setExamplesData(formData);
@@ -30,6 +21,7 @@ function App() {
     <div className="App">
        <button onClick={()=>setActivePage(activePage + 1)}>Click</button>
         <Components essayPrompt={essayPrompt} setEssayPrompt={setEssayPrompt} getEssayExamples={getEssayExamples} activePage={activePage} setActivePage={setActivePage}/>
+        <PromptTemplate examplesData={examplesData} essayPrompt={essayPrompt} gptRequestString={gptRequestString} setGPTRequestString={setGPTRequestString}/>
     </div>
   );
 }
